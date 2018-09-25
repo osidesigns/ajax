8a99b648b5fd99e33e912da78b62a42f16bdb7da
Steven Andrews <osidesigns@gmail.com>
2018-09-25 18:39:33 -0500

    Initial Commit of AJAX code

---
+++ js/user.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+
+		$.ajax({
+
+			url: 'https://jsonplaceholder.typicode.com/users',
+			method: 'GET',
+			success: function(response){
+
+				console.log(response);
+				var users = response;
+				var output = '';
+
+				for(var i = 0; i < users.length; i++){
+					output += '<p>' + users[i].name + '<br />' + users[i].address.street + ' ' +users[i].address.suite + ' ' + users[i].address.city + ' ' + users[i].address.zipcode + '</p>';
+				}
+
+				$('#users').html(output);
+				//console.log(this.responseText);
+			}
+
+		});
+	
+});
+
+
+/*
+var xhr = new XMLHttpRequest();
+var userBtn = document.getElementById('trigger');
+
+userBtn.onclick = function(){
+
+			var users = JSON.parse(xhr.responseText);
+			var output = '';
+
+			for(var i = 0; i < users.length; i++){
+				output += '<p>' + users[i].name + '<br />' + users[i].address.street + ' ' +users[i].address.suite + ' ' + users[i].address.city + ' ' + users[i].address.zipcode + '</p>';
+			}
+
+			document.getElementById('users').innerHTML = output;
+			//console.log(this.responseText);
+		};
+
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+xhr.send();*/
